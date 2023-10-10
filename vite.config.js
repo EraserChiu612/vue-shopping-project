c5e2917f313bcb4ddc7183b10d5a471a30b505b4
binary612 <sa0ka6mo1to2@gmail.com>
2023-10-10 12:15:49 +0800

    路徑設置

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
+    // 實際的路徑轉換 @ -> src
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
