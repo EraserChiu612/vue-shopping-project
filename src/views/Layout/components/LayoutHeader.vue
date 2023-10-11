090e8161c1ff5b22e418a5a1b075429375ac3807
Peter laptop <eraser0612@gmail.com>
2023-10-11 19:00:50 +0800

    level 1 finish

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -23,7 +23,7 @@ const CategoryStore = useCategoryStore()
           <RouterLink to="/">首頁</RouterLink>
         </li>
         <li class="home" v-for="item in CategoryStore.categoryList" :key="item.id">
-          <RouterLink to="/">{{ item.name }}</RouterLink>
+          <RouterLink :to="`/${item.id}`">{{ item.name }}</RouterLink>
         </li>
 
 
